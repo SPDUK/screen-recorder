cf183ab335798f803311d28fb6a831ee7c6eabc1
SPDUK <spdevuk@gmail.com>
2020-03-25 22:35:45 +0000

    refactor: LandingPage -> Recorder

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -7,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      name: 'recorder',
+      component: require('@/components/Recorder').default
     },
     {
       path: '*',
